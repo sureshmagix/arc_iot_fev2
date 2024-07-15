890797d5d2d94f4f31db6a10b3104dd534e2e679
suresh babu <sureshmagix@gmail.com>
2024-07-15 23:41:51 +0530

    Drawer nav alone

--- arc/App.js
+++ arc/App.js
@@ -1,15 +1,19 @@
+import 'react-native-gesture-handler';
 import { Text, StyleSheet, View } from 'react-native';
 import {NavigationContainer, useNavigation, DrawerActions} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+
 import HomeScreen from './src/screens/HomeScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import UserScreen from './src/screens/UserScreen';
 
-const App = () => {
+
+const StackNav=()=>{
   const Stack = createNativeStackNavigator();
-  return (
-<NavigationContainer>
-  <Stack.Navigator initialRouteName='Home' screenOptions={
+  return(
+    <Stack.Navigator initialRouteName='Home' screenOptions={
       {
         statusBarColor:"blue",
         headerTintColor:'white',
@@ -25,6 +29,33 @@ const App = () => {
     <Stack.Screen name='Profile' component = {ProfileScreen} />
     <Stack.Screen name='User' component = {UserScreen} />
   </Stack.Navigator>
+  )
+
+
+}
+
+
+const App = () => {
+  
+    const Drawer = createDrawerNavigator();
+  return (
+<NavigationContainer >
+  <Drawer.Navigator screenOptions={
+      {
+        statusBarColor:"blue",
+        headerTintColor:'white',
+        headerTitleAlign:"center",
+        headerStyle:{
+          backgroundColor:"blue",
+                        
+        }
+      }
+      }>
+    <Drawer.Screen name='Home' component={HomeScreen} />
+    <Drawer.Screen name='Profile' component={ProfileScreen} />
+    <Drawer.Screen name='User' component={UserScreen} />
+  </Drawer.Navigator>
+  
 </NavigationContainer>
   );
 };
